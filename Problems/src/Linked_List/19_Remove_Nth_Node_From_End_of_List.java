732e22e34cae7f6e7e8f12684c6d655f6b2ee3a7
leo81601 <36606875+leo81601@users.noreply.github.com>
2019-06-12 17:23:48 -0400

    Add New Problems Solution

---
+++ Problems/src/Linked_List/19_Remove_Nth_Node_From_End_of_List.java
@@ -0,0 +1,28 @@
+package Linked_List;
+
+/**
+ * Given a linked list, remove the n-th node from the end of list and return its head.
+ *
+ * Related Topics: Linked List, Two Pointers
+ */
+
+class Remove_Nth_Node_From_End_of_List {
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) { val = x; }
+    }
+
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode slow = dummy, fast = dummy;
+        while (fast.next != null) {
+            if (n <= 0) slow = slow.next;
+            fast = fast.next;
+            n--;
+        }
+        if (slow.next != null) slow.next = slow.next.next;
+        return dummy.next;
+    }
+}
---
+++ Problems/src/String/20_Valid_Parentheses.java
@@ -0,0 +1,33 @@
+package String;
+
+import java.util.Stack;
+
+/**
+ * Given a string containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+ *
+ * An input string is valid if:
+ *
+ * Open brackets must be closed by the same type of brackets.
+ * Open brackets must be closed in the correct order.
+ * Note that an empty string is also considered valid.
+ *
+ * Related Topics: String, Stack
+ */
+
+class Valid_Parentheses {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (char c : s.toCharArray()) {
+            if (c == '(') {
+                stack.push(')');
+            }else if (c == '[') {
+                stack.push(']');
+            }else if (c == '{') {
+                stack.push('}');
+            }else if (stack.isEmpty() || stack.pop() != c) {
+                return false;
+            }
+        }
+        return stack.isEmpty();
+    }
+}
