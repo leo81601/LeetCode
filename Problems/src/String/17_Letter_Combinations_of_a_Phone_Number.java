446ce726f90e5df62b16c79f47d3aea16d4b342b
leo81601 <36606875+leo81601@users.noreply.github.com>
2019-06-12 15:52:17 -0400

    Add New Problems Solution

---
+++ Problems/src/String/17_Letter_Combinations_of_a_Phone_Number.java
@@ -0,0 +1,33 @@
+package String;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Given a string containing digits from 2-9 inclusive, return all possible letter combinations that the number could represent.
+ *
+ * A mapping of digit to letters (just like on the telephone buttons) is given below. Note that 1 does not map to any letters.
+ *
+ * Related Topics: String, Backtracking
+ */
+
+class Letter_Combinations_of_a_Phone_Number {
+    private String[] keys = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxzy"};
+    public List<String> letterCombinations(String digits) {
+        List<String> list = new ArrayList<>();
+        if (digits == null || digits.length() == 0) return list;
+        combination("", digits, 0, list);
+        return list;
+    }
+
+    private void combination(String letter, String number, int index, List<String> list) {
+        if (index >= number.length()) {
+            list.add(letter);
+            return;
+        }
+        String letters = keys[(number.charAt(index) - '0')];
+        for (int i = 0; i < letters.length(); i++) {
+            combination(letter + letters.charAt(i), number, index + 1, list);
+        }
+    }
+}
---
+++ Problems/src/Two_Pointers/18_4Sum.java
@@ -0,0 +1,44 @@
+package Two_Pointers;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Given an array nums of n integers and an integer target, are there elements a, b, c, and d in nums such that a + b + c + d = target? Find all unique quadruplets in the array which gives the sum of target.
+ *
+ * Note: The solution set must not contain duplicate quadruplets.
+ *
+ * Related Topics: Array, Hash Table, Two Pointers
+ */
+
+class Four_Sum {
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+        List<List<Integer>> list = new ArrayList<>();
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length - 3; i++) {
+            if (i == 0 || nums[i] != nums[i - 1]) {
+                for (int j = i + 1; j < nums.length - 2; j++) {
+                    if (j == i + 1 || nums[j] != nums[j - 1]) {
+                        int left = j + 1, right = nums.length - 1;
+                        while (left < right) {
+                            int sum = nums[i] + nums[j] + nums[left] + nums[right];
+                            if (sum == target) {
+                                list.add(Arrays.asList(nums[i], nums[j], nums[left], nums[right]));
+                                left++;
+                                right--;
+                                while (left < right && nums[left] == nums[left - 1]) left++;
+                                while (left < right && nums[right] == nums[right + 1]) right--;
+                            }else if (sum < target) {
+                                left++;
+                            }else {
+                                right--;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return list;
+    }
+}
